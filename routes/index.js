630d6fbfce3aa9c115a1388efb35c01deb9cd645
Michael Donahue <michaeldonahue17@gmail.com>
2020-02-21 20:54:32 -0500

    Added Spotify Connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
 const fs = require('fs');
 const readline = require('readline');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
+
+var request = require('request');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/drive'];
@@ -11,15 +13,6 @@ const SCOPES = ['https://www.googleapis.com/auth/drive'];
 // created automatically when the authorization flow completes for the first
 // time.
 const TOKEN_PATH = 'token.json';
-
-
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), listFiles);
-});
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -29,9 +22,9 @@ fs.readFile('credentials.json', (err, content) => {
  */
 function authorize(credentials, callback) {
   console.log(credentials);
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
@@ -41,106 +34,81 @@ function authorize(credentials, callback) {
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listFiles(auth) {
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name} (${file.id})`);
-      });
-    } else {
-      console.log('No files found.');
-    }
-  });
-}
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.get('/stories', async function(req, res, next) {
+router.get('/stories', async function (req, res, next) {
 
   fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive API.
-    authorize(JSON.parse(content), async function(auth) {
-      const drive = google.drive({version: 'v3', auth});
+    authorize(JSON.parse(content), async function (auth) {
+      const drive = google.drive({ version: 'v3', auth });
       drive.files.list({
         q: "parents in '1CAQlXNXi3gQf-Z39pNHzT7gyChrHv-B_'"
-      }).then(async function(value) {
+      }).then(async function (value) {
         list = await getValues(drive, value)
         res.status(200).json(list)
       })
     })
   });
-  
+
 
 });
 
 async function getValues(drive, value) {
   let list = [];
-  for(let i = 0; i < value.data.files.length; i++) {
-    let v = {doc: null, meta: null};
-    if(value.data.files[i].mimeType === "application/vnd.google-apps.document") {
+  for (let i = 0; i < value.data.files.length; i++) {
+    let v = { doc: null, meta: null };
+    if (value.data.files[i].mimeType === "application/vnd.google-apps.document") {
       v.doc = await drive.files.export({
         fileId: value.data.files[i].id,
         mimeType: "text/html"
       })
-      v.meta = await drive.files.get({fileId: value.data.files[i].id})
+      v.meta = await drive.files.get({ fileId: value.data.files[i].id })
       list.push(v);
-    } 
+    }
 
   }
-  
-  // value.data.files.map(async function(f) {
-  //   let ret = await drive.files.get({fileId: f.id})
-  //   list.push(ret)
-  // })
-  //list.push(v)
   return list
 }
+
+/*Spotify authorization*/
+var client_id = 'a3b7800b71e74934aa288bc3846a8f61'; // Your client id
+var client_secret = '48822fdd901345259f57d4e2e2e2726d'; // Your secret
+
+router.get('/spotifyPlaylists', function(req, res, next) {
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+  
+      // use the access token to access the Spotify Web API
+      var token = body.access_token;
+      var options = {
+        url: 'https://api.spotify.com/v1/users/oecs54niyzomumcfvuq5v83ws/playlists',
+        headers: {
+          'Authorization': 'Bearer ' + token
+        },
+        json: true
+      };
+      request.get(options, function(error, response, body) {
+        res.status(200).json(response)
+      });
+    }
+  });
+})
 
 
 module.exports = router;
